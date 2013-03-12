71cabc586a32df10be0c85155903327c12746152
Daniel <daniel@daniel-sandbox.(none)>
2013-03-12 00:07:32 -0400

    -Completed the AsyncTask body, code should compile and run now. -However, RetrieveFeed should most likely return something.

--- src/com/example/srssreader/MainActivity.java
+++ src/com/example/srssreader/MainActivity.java
@@ -14,80 +14,76 @@ import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
 public class MainActivity extends Activity {
-    Button btn;
+	Button btn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        btn = (Button) findViewById(R.id.go);
-        
-        btn.setOnClickListener(new Button.OnClickListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                fetchRSSFeed();
-            }
+		btn = (Button) findViewById(R.id.go);
 
-            private void fetchRSSFeed() {
-                // TODO Auto-generated method stub
-                HttpClient hc = new DefaultHttpClient();
-                HttpResponse response = null;
-                String content = "";
-                
-                try {
-                    //HttpPost httppost = new HttpPost("http://www.wretch.cc/blog/stanely5&rss20=1");
-                    HttpGet httpget = new HttpGet("http://www.wretch.cc/blog/stanely5&rss20=1");
-                    response = hc.execute(httpget);
-                    if(response != null) {
-                        Log.d("stanely", "response != null");
-                    } else
-                        Log.d("stanely", "response == null");
-                    
-                    content += EntityUtils.toString(response.getEntity());
-                }
-                
-                catch (ClientProtocolException ex) {
-                    Log.d("stanely", "Catch ClientProtocolException!!!");
-                }
-                
-                catch (IOException ex) {
-                    Log.d("stanely", "Catch IOException!!!");
-                }
-                
-                finally {
-                    hc.getConnectionManager().shutdown();
-                }
-                
-            }
-            
+		btn.setOnClickListener(new Button.OnClickListener() {
 
-            
-        });
-    }
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				fetchRSSFeed();
+			}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		});
+
+	}
+
+	private void fetchRSSFeed() {
+		new RetrieveFeedAsync().execute();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
 }
 
-class RetrieveFeedAsync extends AsyncTask<String, Void, String> {
+class RetrieveFeedAsync extends AsyncTask<Void, Void, Void> {
 
 	@Override
-	protected String doInBackground(String... params) {
-		// TODO Auto-generated method stub
+	protected Void doInBackground(Void... params) {
+		HttpClient hc = new DefaultHttpClient();
+		HttpResponse response = null;
+		try {
+			// HttpPost httppost = new
+			// HttpPost("http://www.wretch.cc/blog/stanely5&rss20=1");
+			HttpGet httpget = new HttpGet(
+					"http://www.wretch.cc/blog/stanely5&rss20=1");
+			response = hc.execute(httpget);
+			if (response != null) {
+				Log.d("stanely", "response != null");
+			} else
+				Log.d("stanely", "response == null");
+
+			EntityUtils.toString(response.getEntity());
+		}
+
+		catch (ClientProtocolException ex) {
+			Log.d("stanely", "Catch ClientProtocolException!!!");
+		}
+
+		catch (IOException ex) {
+			Log.d("stanely", "Catch IOException!!!");
+		}
+
+		finally {
+			hc.getConnectionManager().shutdown();
+		}
 		return null;
 	}
-	
+
 }
