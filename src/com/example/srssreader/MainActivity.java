89fee4b7777126e5d7b6beefa9e0e8341b9f6e15
stanely <stanely@gmail.com>
2013-03-13 11:18:03 +0800

    Merge Daniel's second fix (71cabc5) and handle the conflict codes.

--- src/com/example/srssreader/MainActivity.java
+++ src/com/example/srssreader/MainActivity.java
@@ -16,7 +16,6 @@ import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
@@ -28,7 +27,7 @@ public class MainActivity extends Activity {
     
     //String xmlURL = "http://www.wretch.cc/blog/stanely5&rss20=1";
     String xmlURL = "http://rss.cnn.com/rss/edition_asia.rss";
-    private ArrayList<RSSItemField> item;
+    public ArrayList<RSSItemField> item;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,57 +44,10 @@ public class MainActivity extends Activity {
                 // TODO Auto-generated method stub
                 fetchRSSFeed();
             }
-
-            private void fetchRSSFeed() {
-                // TODO Auto-generated method stub
-                HttpClient hc = new DefaultHttpClient();
-                HttpResponse response = null;
-                String content = "";
-                
-                try {
-                    //HttpPost httppost = new HttpPost("http://www.wretch.cc/blog/stanely5&rss20=1");
-                    HttpGet httpget = new HttpGet(xmlURL);
-                    response = hc.execute(httpget);
-                    
-                    if(response != null) {
-                        Log.d("stanely", "response != null");
-                    } else
-                        Log.d("stanely", "response == null");
-                    
-                    content += EntityUtils.toString(response.getEntity());  // get xml file now
-                    
-                    // parsing
-                    XmlParser p = new XmlParser();
-                    item = p.parseXmlResponse(content);
-                    
-                    Log.d("stanely", "Ending!!!");
-                }
-                
-                catch (ClientProtocolException ex) {
-                    Log.d("stanely", "Catch ClientProtocolException!!!");
-                }
-                
-                catch (IOException ex) {
-                    Log.d("stanely", "Catch IOException!!!");
-                }
-                
-                finally {
-                    hc.getConnectionManager().shutdown();
-                    
-                    text.setText(new String("Count:" + item.size()));
-                    
-                    for(int i = 0; i < item.size(); i++) {
-                        Log.d("stanely", item.get(i).title);
-                    }
-                    
-                }
-                
-            }
-            
-
-            
         });
-    }
+        
+        // display on ListView
+    }         
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -104,14 +56,75 @@ public class MainActivity extends Activity {
         return true;
     }
 
-}
 
-class RetrieveFeedAsync extends AsyncTask<String, Void, String> {
-
-	@Override
-	protected String doInBackground(String... params) {
-		// TODO Auto-generated method stub
-		return null;
+	private void fetchRSSFeed() {
+		new RetrieveFeedAsync().execute(xmlURL);
 	}
-	
+
+
+    class RetrieveFeedAsync extends AsyncTask<String, Integer, String> {
+    
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient hc = new DefaultHttpClient();
+            HttpResponse response = null;
+            String content = "";
+            
+            try {
+                //HttpPost httppost = new HttpPost("http://www.wretch.cc/blog/stanely5&rss20=1");
+                //HttpGet httpget = new HttpGet(xmlURL);
+                HttpGet httpget = new HttpGet(params[0]);  // get the given URL from caller
+                response = hc.execute(httpget);
+                
+                if(response != null) {
+                    Log.d("stanely", "response != null");
+                } else
+                    Log.d("stanely", "response == null");
+                
+                content += EntityUtils.toString(response.getEntity());  // get xml file now
+                
+                // parsing
+                XmlParser p = new XmlParser();
+                item = p.parseXmlResponse(content);
+                
+                Log.d("stanely", "Ending!!!" + "size = " + item.size());
+            }
+            
+            catch (ClientProtocolException ex) {
+                Log.d("stanely", "Catch ClientProtocolException!!!");
+                return null;
+            }
+            
+            catch (IOException ex) {
+                Log.d("stanely", "Catch IOException!!!");
+                return null;
+            }
+            
+            finally {
+                hc.getConnectionManager().shutdown();
+                
+                //text.setText(new String("Count:" + item.size()));  // TODO: Will crash, check later.
+                
+                for(int i = 0; i < item.size(); i++) {
+                    Log.d("stanely", item.get(i).title);
+                }
+                
+            }
+          
+    		return "SUCCESS";
+    	}
+    
+        @Override
+        protected void onPostExecute(String result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);    
+        }
+    
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            // TODO Auto-generated method stub
+            super.onProgressUpdate(values);
+        }
+    }
+
 }
